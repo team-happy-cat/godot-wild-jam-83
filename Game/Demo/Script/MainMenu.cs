fb336a1ca0327cc79c32813b730649aa4dab0e36
AzuWo <84492361+AzuWo-MN@users.noreply.github.com>
2025-07-12 22:01:52 +0200

    Added blocked out levels 1 to 3 and level selector to main menu

--- Game/Demo/Script/MainMenu.cs
+++ Game/Demo/Script/MainMenu.cs
@@ -3,20 +3,44 @@ using Godot;
 
 public partial class MainMenu : Control
 {
-    [Export] public Button StartButton;
-    
-    private LevelManager levelManager;
+	[Export] public Button StartButton;
+	
+	// Debug - to be removed for release
+	[Export] public Button LevelButton1;
+	[Export] public Button LevelButton2;
+	[Export] public Button LevelButton3;
+	
+	private LevelManager levelManager;
 
-    public override void _Ready()
-    {
-        StartButton.Pressed += OnStartButtonPressed;
-        levelManager = GetNode<LevelManager>("/root/LevelManager");
-    }
+	public override void _Ready()
+	{
+		StartButton.Pressed += OnStartButtonPressed;
+		
+		// Debug - to be removed for release
+		LevelButton1.Pressed += OnLevelButton1Pressed;
+		LevelButton2.Pressed += OnLevelButton2Pressed;
+		LevelButton3.Pressed += OnLevelButton3Pressed;
+		
+		levelManager = GetNode<LevelManager>("/root/LevelManager");
+	}
 
-    private void OnStartButtonPressed()
-    {
-        levelManager.ChangeLevel("Level_01", "SP_Level_01");
-    }
+	private void OnStartButtonPressed()
+	{
+		levelManager.ChangeLevel("Level_01", "SP_Level_01");
+	}
 
+	private void OnLevelButton1Pressed()
+	{
+		levelManager.ChangeLevel("Level_01", "SP_Level_01");
+	}
+	
+	private void OnLevelButton2Pressed()
+	{
+		levelManager.ChangeLevel("Level_02", "SP_Level_02");
+	}
+	
+	private void OnLevelButton3Pressed()
+	{
+		levelManager.ChangeLevel("Level_03", "SP_Level_03");
+	}
 } 
-
--- Game/addons/StandardAssets/Script/Audio/SFX.cs
+++ Game/addons/StandardAssets/Script/Audio/SFX.cs
@@ -3,79 +3,78 @@ using System.Collections.Generic;
 
 namespace Game
 {
-    /// <summary>
-    /// Autoload singleton accessible at /root/SFX
-    /// </summary>
-    public partial class SFX : Node
-    {
-        public float MasterVolume { get; set; } = 0.7f;
-        public Dictionary<string, SoundGroup3D> SoundGroups = new();
+	/// <summary>
+	/// Autoload singleton accessible at /root/SFX
+	/// </summary>
+	public partial class SFX : Node
+	{
+		public float MasterVolume { get; set; } = 0.7f;
+		public Dictionary<string, SoundGroup3D> SoundGroups = new();
 
-        private CameraBridge cameraBridge;
+		private CameraBridge cameraBridge;
 
-        public override void _Ready()
-        {
-            cameraBridge = GetNode<CameraBridge>("/root/CameraBridge");
+		public override void _Ready()
+		{
+			cameraBridge = GetNode<CameraBridge>("/root/CameraBridge");
 
-            FindSoundGroups(this);
+			FindSoundGroups(this);
 
-            foreach (SoundGroup3D soundGroup in SoundGroups.Values)
-            {
-                soundGroup.Initialize(this);
-            }
+			foreach (SoundGroup3D soundGroup in SoundGroups.Values)
+			{
+				soundGroup.Initialize(this);
+			}
 
-            GD.PrintRich($"[SFX] [color={ColorsHex.MediumSeaGreen}]Ready[/color] with {SoundGroups.Count} sound groups");
-        }
+			GD.PrintRich($"[SFX] [color={ColorsHex.MediumSeaGreen}]Ready[/color] with {SoundGroups.Count} sound groups");
+		}
 
-        private void FindSoundGroups(Node node)
-        {
-            foreach (Node child in node.GetChildren())
-            {
-                if (child is SoundGroup3D soundGroup)
-                {
-                    SoundGroups[child.Name] = soundGroup;
-                }
-                else
-                {
-                    FindSoundGroups(child);
-                }
-            }
-        }
+		private void FindSoundGroups(Node node)
+		{
+			foreach (Node child in node.GetChildren())
+			{
+				if (child is SoundGroup3D soundGroup)
+				{
+					SoundGroups[child.Name] = soundGroup;
+				}
+				else
+				{
+					FindSoundGroups(child);
+				}
+			}
+		}
 
-        public void Play(string soundGroupName)
-        {
-            PlaySound(soundGroupName, cameraBridge.CameraPosition);
-        }
+		public void Play(string soundGroupName)
+		{
+			PlaySound(soundGroupName, cameraBridge.CameraPosition);
+		}
 
-        public void PlaySound(string soundGroupName)
-        {
-            PlaySound(soundGroupName, cameraBridge.CameraPosition);
-        }
+		public void PlaySound(string soundGroupName)
+		{
+			PlaySound(soundGroupName, cameraBridge.CameraPosition);
+		}
 
-        public void PlaySound(string soundGroupName, Vector3 location)
-        {
-            if (SoundGroups.TryGetValue(soundGroupName, out SoundGroup3D soundGroup))
-            {
-                AudioStreamPlayer3D source = soundGroup.GetAvailableSource();
+		public void PlaySound(string soundGroupName, Vector3 location)
+		{
+			if (SoundGroups.TryGetValue(soundGroupName, out SoundGroup3D soundGroup))
+			{
+				AudioStreamPlayer3D source = soundGroup.GetAvailableSource();
 
-                if (source != null)
-                {
-                    if (source.Playing)
-                    {
-                        GD.Print("Sound group is already playing");
-                    }
-                    GD.Print("[SFX] Playing: " + soundGroupName);
-                    source.Position = location;
-                    source.Play();
-                }
-            }
-            else
-            {
-                GD.Print("[SFX] Requested a sound group that does not exist: " + soundGroupName);
-            }
-        }
+				if (source != null)
+				{
+					if (source.Playing)
+					{
+						GD.Print("Sound group is already playing");
+					}
+					GD.Print("[SFX] Playing: " + soundGroupName);
+					source.Position = location;
+					source.Play();
+				}
+			}
+			else
+			{
+				GD.Print("[SFX] Requested a sound group that does not exist: " + soundGroupName);
+			}
+		}
 
-    }
+	}
 
 }
-
