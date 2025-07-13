864cf5186b6abc114466e9c061e4b5f8328c2591
Gabriel Araújo <windgamedevelopment@gmail.com>
2025-07-12 21:22:48 -0300

    Fix SFX2D not working

--- Game/addons/StandardAssets/Script/Audio/SFX2D.cs
+++ Game/addons/StandardAssets/Script/Audio/SFX2D.cs
@@ -4,46 +4,62 @@ using System.Collections.Generic;
 namespace Game
 {
     /// <summary>
-    /// Autoload singleton accessible at /root/SFX2D
+    /// Autoload singleton accessible at /root/SFX
     /// </summary>
     public partial class SFX2D : Node
     {
         private static SFX2D _instance;
         
-        public static Dictionary<string, AudioStreamPlayer> SoundGroups = new();
+        public static float MasterVolume { get; set; } = 0.7f;
+        public static Dictionary<string, SoundGroup2D> SoundGroups = new();
 
         public override void _Ready()
         {
             _instance = this;
             
-            foreach (Node child in GetChildren())
+            FindSoundGroups(this);
+
+            foreach (SoundGroup2D soundGroup in SoundGroups.Values)
             {
-                if (child is AudioStreamPlayer player)
-                {
-                    SoundGroups[player.Name] = player;
-                }
+                soundGroup.Initialize(this);
             }
 
-            GD.PrintRich($"[SFX2D] [color={ColorsHex.MediumSeaGreen}]Ready[/color] with {SoundGroups.Count} sound groups");
+            GD.PrintRich($"[SFX] [color={ColorsHex.MediumSeaGreen}]Ready[/color] with {SoundGroups.Count} sound groups");
         }
 
-        public static void PlaySound(string soundGroupName)
+        private static void FindSoundGroups(Node node)
         {
-            if (SoundGroups.TryGetValue(soundGroupName, out AudioStreamPlayer player))
+            foreach (Node child in node.GetChildren())
             {
-                if (player != null)
+                if (child is SoundGroup2D soundGroup)
+                    SoundGroups[child.Name] = soundGroup;
+                else FindSoundGroups(child);
+            }
+        }
+
+        public static void PlaySound(string soundGroupName, float volume = 1f)
+        {
+            if (SoundGroups.TryGetValue(soundGroupName, out SoundGroup2D soundGroup))
+            {
+                AudioStreamPlayer source = soundGroup.GetAvailableSource();
+
+                if (source != null)
                 {
-                    GD.Print("[SFX2D] Playing: " + soundGroupName);
-                    player.Play();
+                    if (source.Playing)
+                    {
+                        GD.Print("Sound group is already playing");
+                    }
+                    GD.Print("[SFX] Playing: " + soundGroupName);
+                    source.VolumeLinear = MasterVolume * volume;
+                    source.Play();
                 }
             }
             else
             {
-                GD.Print("[SFX2D] Requested a sound group that does not exist: " + soundGroupName);
+                GD.Print("[SFX] Requested a sound group that does not exist: " + soundGroupName);
             }
         }
 
     }
 
 }
-
