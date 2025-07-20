df655df9bcbbb6510e156dcc71b6c1f012e3a40c
AzuWo <84492361+AzuWo-MN@users.noreply.github.com>
2025-07-20 13:22:01 +0200

    added goose death and attack sound

--- Game/Demo/Script/BellyDisplay.cs
+++ Game/Demo/Script/BellyDisplay.cs
@@ -2,45 +2,48 @@ using Godot;
 
 public enum FoodType
 {
-    Chicken,
-    Mosquito
+	Chicken,
+	Mosquito,
+	Goose
 }
 
 public partial class BellyDisplay : TextureRect
 {
-    [Export] public TextureRect ChickenTexture;
-    [Export] public TextureRect MosquitoTexture;
+	[Export] public TextureRect ChickenTexture;
+	[Export] public TextureRect MosquitoTexture;
 
-    public override void _Ready()
-    {
-        ChickenTexture.Visible = false;
-        MosquitoTexture.Visible = false;
-    }
+	public override void _Ready()
+	{
+		ChickenTexture.Visible = false;
+		MosquitoTexture.Visible = false;
+	}
 
-    public void Full(FoodType foodType)
-    {
-        ChickenTexture.Visible = false;
-        MosquitoTexture.Visible = false;
+	public void Full(FoodType foodType)
+	{
+		ChickenTexture.Visible = false;
+		MosquitoTexture.Visible = false;
 
-        switch (foodType)
-        {
-            case FoodType.Chicken:
-                ChickenTexture.Visible = true;
-                break;
-            case FoodType.Mosquito:
-                MosquitoTexture.Visible = true;
-                break;
-        }
+		switch (foodType)
+		{
+			case FoodType.Chicken:
+				ChickenTexture.Visible = true;
+				break;
+			case FoodType.Goose:
+				ChickenTexture.Visible = true;
+				break;
+			case FoodType.Mosquito:
+				MosquitoTexture.Visible = true;
+				break;
+		}
 
-        GD.Print("[BellyDisplay] Set to full");
-    }
+		GD.Print("[BellyDisplay] Set to full");
+	}
 
-    public void Empty()
-    {
-        ChickenTexture.Visible = false;
-        MosquitoTexture.Visible = false;
-        GD.Print("[BellyDisplay] Set to empty");
-    }
+	public void Empty()
+	{
+		ChickenTexture.Visible = false;
+		MosquitoTexture.Visible = false;
+		GD.Print("[BellyDisplay] Set to empty");
+	}
 
 }
-
