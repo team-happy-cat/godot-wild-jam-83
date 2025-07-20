1885e7649265f00313bd15632889230b5c45174e
AzuWo <84492361+AzuWo-MN@users.noreply.github.com>
2025-07-20 05:07:34 +0200

    added game over music

--- Game/Demo/UI/Autoload/GameOver.cs
+++ Game/Demo/UI/Autoload/GameOver.cs
@@ -3,44 +3,46 @@ using Godot;
 
 public partial class GameOver : CanvasLayer
 {
-    private static GameOver _instance;
-    private static LevelManager _levelManager;
+	private static GameOver _instance;
+	private static LevelManager _levelManager;
 
-    [Export] public AnimationPlayer Animations;
+	[Export] public AnimationPlayer Animations;
+	[Export] public AudioStreamPlayer GameOverAudio;
 
-    [ExportSubgroup("Buttons", "_button")]
-    [Export] private TextureButton _buttonExit;
-    [Export] private TextureButton _buttonRestart;
-    
-    public override void _Ready()
-    {
-        base._Ready();
-        
-        _instance = this;
-        _levelManager = GetNode<LevelManager>("/root/LevelManager");
-        
-        _buttonExit.Pressed += () =>
-        {
-            _levelManager.ChangeLevel("Main_Menu");
-            Close();
-        };
-        _buttonRestart.Pressed += () =>
-        {
-            _levelManager.ChangeLevel(_levelManager.CurrentLevelID, "SP_" + _levelManager.CurrentLevelID);
-            Close();
-        };
-    }
+	[ExportSubgroup("Buttons", "_button")]
+	[Export] private TextureButton _buttonExit;
+	[Export] private TextureButton _buttonRestart;
+	
+	public override void _Ready()
+	{
+		base._Ready();
+		
+		_instance = this;
+		_levelManager = GetNode<LevelManager>("/root/LevelManager");
+		
+		_buttonExit.Pressed += () =>
+		{
+			_levelManager.ChangeLevel("Main_Menu");
+			Close();
+		};
+		_buttonRestart.Pressed += () =>
+		{
+			_levelManager.ChangeLevel(_levelManager.CurrentLevelID, "SP_" + _levelManager.CurrentLevelID);
+			Close();
+		};
+	}
 
-    public static void Open()
-    {
-        _instance.Animations.Play("open");
-        Mouse.SetVisible();
-        _instance.GetTree().Paused = true;
-    }
+	public static void Open()
+	{
+		_instance.Animations.Play("open");
+		Mouse.SetVisible();
+		_instance.GetTree().Paused = true;
+		_instance.GameOverAudio.Play();
+	}
 
-    public static void Close()
-    {
-        _instance.Animations.Play("close");
-        _instance.GetTree().Paused = false;
-    }
+	public static void Close()
+	{
+		_instance.Animations.Play("close");
+		_instance.GetTree().Paused = false;
+	}
 }
