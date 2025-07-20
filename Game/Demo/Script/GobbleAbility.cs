8bd37ee97faeac80fd782d5157439592d592ed08
AzuWo <84492361+AzuWo-MN@users.noreply.github.com>
2025-07-20 05:25:45 +0200

    updated enrironment to remove floating trees, removed collision with player body for flies and removed undergraound chicken on level 2

--- Game/Demo/Script/GobbleAbility.cs
+++ Game/Demo/Script/GobbleAbility.cs
@@ -2,30 +2,30 @@ using Godot;
 
 public partial class GobbleAbility : Area3D
 {
-    [Export] public EatAndLungeAbility eatAndLungeAbility;
+	[Export] public EatAndLungeAbility eatAndLungeAbility;
 
-    public override void _Ready()
-    {
-        BodyEntered += OnBodyEntered;
-    }
+	public override void _Ready()
+	{
+		BodyEntered += OnBodyEntered;
+	}
 
-    public override void _ExitTree()
-    {
-        BodyEntered -= OnBodyEntered;
-    }
+	public override void _ExitTree()
+	{
+		BodyEntered -= OnBodyEntered;
+	}
 
-    private void OnBodyEntered(Node3D body)
-    {
-        GD.Print("[GobbleAbility] body entered");
-        if (body is AgentBase agentBase)
-        {
-            GD.Print("[GobbleAbility] Agent base is true");
-            if (eatAndLungeAbility.Gobbling)
-            {
-                agentBase.Die();
-                GD.Print("[GobbleAbility] Gobbled: ", agentBase.Name);
-            }
-        }
-    }
+	private void OnBodyEntered(Node3D body)
+	{
+		GD.Print("[GobbleAbility] body entered");
+		if (body is AgentBase agentBase)
+		{
+			GD.Print("[GobbleAbility] Agent base is true");
+			if (eatAndLungeAbility.Gobbling)
+			{
+				agentBase.Die();
+				GD.Print("[GobbleAbility] Gobbled: ", agentBase.Name);
+			}
+		}
+	}
 
 }
