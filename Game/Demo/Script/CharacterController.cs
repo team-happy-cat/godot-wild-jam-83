2f1e74f87f2aa18aa1c60cb6938fa56573d4d666
AzuWo <84492361+AzuWo-MN@users.noreply.github.com>
2025-07-20 05:47:59 +0200

    Changed lunge behavior, added air movement & hid frog mesh

--- Game/Demo/Script/CharacterController.cs
+++ Game/Demo/Script/CharacterController.cs
@@ -2,126 +2,165 @@ using Godot;
 
 namespace Game
 {
-    public partial class CharacterController : CharacterBody3D
-    {
-        [ExportCategory("Character")]
-        [Export] public float BaseSpeed = 5.0f;
-        [Export] public float SprintSpeed = 8.0f;
-        [Export] public float JumpVelocity = 4.5f;
+	public partial class CharacterController : CharacterBody3D
+	{
+		[ExportCategory("Character")]
+		[Export] public float BaseSpeed = 5.0f;
+		[Export] public float SprintSpeed = 8.0f;
+		[Export] public float JumpVelocity = 4.5f;
+		
+		// MN air control 
+		[Export] public float AirSpeed = 2.0f;
 
-        [ExportGroup("Controls")]
-        [Export] public string Jump = "jump";
-        [Export] public string Left = "move_left";
-        [Export] public string Right = "move_right";
-        [Export] public string Forward = "move_forward";
-        [Export] public string Backward = "move_backward";
-        [Export] public string Pause = "start";
-        [Export] public string Sprint = "sprint";
+		[ExportGroup("Controls")]
+		[Export] public string Jump = "jump";
+		[Export] public string Left = "move_left";
+		[Export] public string Right = "move_right";
+		[Export] public string Forward = "move_forward";
+		[Export] public string Backward = "move_backward";
+		[Export] public string Pause = "start";
+		[Export] public string Sprint = "sprint";
 
-        [Export] public EatAndLungeAbility eatAndLungeAbility;
+		[Export] public EatAndLungeAbility eatAndLungeAbility;
 
-        private float gravity;
-        private float speed;
-        private CameraBridge cameraBridge;
+		private float gravity;
+		private float speed;
+		private CameraBridge cameraBridge;
 
-        private Vector3 lastGroundedVelocity = Vector3.Zero;
-        private bool groundedLastFrame = false;
+		private Vector3 lastGroundedVelocity = Vector3.Zero;
+		private bool groundedLastFrame = false;
 
-        public bool InWater { get; set; } = false;
+		public bool InWater { get; set; } = false;
+		
+		// MN air control
+		public bool IsLunging { get; set; } = false;
 
-        private LevelManager levelManager;
+		private LevelManager levelManager;
 
-        public override void _Ready()
-        {
-            gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
-            speed = BaseSpeed;
-            cameraBridge = GetNode<CameraBridge>("/root/CameraBridge");
-            Mouse.SetCaptured("CharacterController");
-            levelManager = GetNode<LevelManager>("/root/LevelManager");
-        }
+		public override void _Ready()
+		{
+			gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+			speed = BaseSpeed;
+			cameraBridge = GetNode<CameraBridge>("/root/CameraBridge");
+			Mouse.SetCaptured("CharacterController");
+			levelManager = GetNode<LevelManager>("/root/LevelManager");
+		}
 
-        public override void _PhysicsProcess(double delta)
-        {
-            if (eatAndLungeAbility.Gobbling)
-            {
-                Velocity = new Vector3(Velocity.X, Velocity.Y - gravity * (float)delta, Velocity.Z);
-                MoveAndSlide();
-                return;
-            }
+		public override void _PhysicsProcess(double delta)
+		{
+			if (eatAndLungeAbility.Gobbling)
+			{
+				Velocity = new Vector3(Velocity.X, Velocity.Y - gravity * (float)delta, Velocity.Z);
+				MoveAndSlide();
+				return;
+			}
 
-            if (!IsOnFloor())
-            {
-                Velocity = new Vector3(Velocity.X, Velocity.Y - gravity * (float)delta, Velocity.Z);
-            }
+			if (!IsOnFloor())
+			{
+				Velocity = new Vector3(Velocity.X, Velocity.Y - gravity * (float)delta, Velocity.Z);
+				
+				if (!IsLunging)
+				{
+					speed = AirSpeed;
+					
+					Vector2 inputDir = Input.GetVector(Left, Right, Forward, Backward);
+					Vector3 cameraForward = cameraBridge.MainCamera.GlobalTransform.Basis.Z;
+					Vector3 cameraRight = cameraBridge.MainCamera.GlobalTransform.Basis.X;
 
-            if (Input.IsActionJustPressed(Jump) && IsOnFloor())
-            {
-                Velocity = new Vector3(Velocity.X, JumpVelocity, Velocity.Z);
-            }
+					cameraForward.Y = 0;
+					cameraRight.Y = 0;
+					cameraForward = cameraForward.Normalized();
+					cameraRight = cameraRight.Normalized();
 
-            if (IsOnFloor())
-            {
-                if (!groundedLastFrame)
-                {
-                    lastGroundedVelocity = Vector3.Zero;
-                }
-                groundedLastFrame = true;
+					Vector3 direction = (cameraForward * inputDir.Y + cameraRight * inputDir.X).Normalized();
 
-                speed = Input.IsActionPressed(Sprint) ? SprintSpeed : BaseSpeed;
+					if (direction != Vector3.Zero)
+					{
+						Velocity = new Vector3(direction.X * speed, Velocity.Y, direction.Z * speed);
 
-                if (InWater)
-                {
-                    speed *= 0.5f;
-                }
+						float targetAngle = Mathf.Atan2(direction.X, direction.Z);
+						Rotation = new Vector3(0, targetAngle, 0);
+					}
+					else
+					{
+						Velocity = new Vector3(
+							Mathf.MoveToward(Velocity.X, 0, speed),
+							Velocity.Y,
+							Mathf.MoveToward(Velocity.Z, 0, speed)
+						);
+					}
+				}
+			}
 
-                Vector2 inputDir = Input.GetVector(Left, Right, Forward, Backward);
-                Vector3 cameraForward = cameraBridge.MainCamera.GlobalTransform.Basis.Z;
-                Vector3 cameraRight = cameraBridge.MainCamera.GlobalTransform.Basis.X;
+			if (Input.IsActionJustPressed(Jump) && IsOnFloor())
+			{
+				Velocity = new Vector3(Velocity.X, JumpVelocity, Velocity.Z);
+			}
 
-                cameraForward.Y = 0;
-                cameraRight.Y = 0;
-                cameraForward = cameraForward.Normalized();
-                cameraRight = cameraRight.Normalized();
+			if (IsOnFloor())
+			{
+				if (!groundedLastFrame)
+				{
+					lastGroundedVelocity = Vector3.Zero;
+				}
+				groundedLastFrame = true;
 
-                Vector3 direction = (cameraForward * inputDir.Y + cameraRight * inputDir.X).Normalized();
+				speed = Input.IsActionPressed(Sprint) ? SprintSpeed : BaseSpeed;
 
-                if (direction != Vector3.Zero)
-                {
-                    Velocity = new Vector3(direction.X * speed, Velocity.Y, direction.Z * speed);
+				if (InWater)
+				{
+					speed *= 0.5f;
+				}
 
-                    float targetAngle = Mathf.Atan2(direction.X, direction.Z);
-                    Rotation = new Vector3(0, targetAngle, 0);
-                }
-                else
-                {
-                    Velocity = new Vector3(
-                        Mathf.MoveToward(Velocity.X, 0, speed),
-                        Velocity.Y,
-                        Mathf.MoveToward(Velocity.Z, 0, speed)
-                    );
-                }
-            }
-            else
-            {
-                if (groundedLastFrame)
-                {
-                    lastGroundedVelocity = new Vector3(Velocity.X, 0, Velocity.Z);
-                }
-                groundedLastFrame = false;
+				Vector2 inputDir = Input.GetVector(Left, Right, Forward, Backward);
+				Vector3 cameraForward = cameraBridge.MainCamera.GlobalTransform.Basis.Z;
+				Vector3 cameraRight = cameraBridge.MainCamera.GlobalTransform.Basis.X;
 
-                Velocity = new Vector3(lastGroundedVelocity.X, Velocity.Y, lastGroundedVelocity.Z);
-            }
+				cameraForward.Y = 0;
+				cameraRight.Y = 0;
+				cameraForward = cameraForward.Normalized();
+				cameraRight = cameraRight.Normalized();
 
-            MoveAndSlide();
-        }
+				Vector3 direction = (cameraForward * inputDir.Y + cameraRight * inputDir.X).Normalized();
 
-        public void Die()
-        {
-            GD.Print("[Player] Died!");
-            GameOver.Open();
-            // levelManager.ChangeLevel("Main_Menu");
-        }
-    }
+				if (direction != Vector3.Zero)
+				{
+					Velocity = new Vector3(direction.X * speed, Velocity.Y, direction.Z * speed);
+
+					float targetAngle = Mathf.Atan2(direction.X, direction.Z);
+					Rotation = new Vector3(0, targetAngle, 0);
+				}
+				else
+				{
+					Velocity = new Vector3(
+						Mathf.MoveToward(Velocity.X, 0, speed),
+						Velocity.Y,
+						Mathf.MoveToward(Velocity.Z, 0, speed)
+					);
+				}
+			}
+			else
+			{
+				/*
+				if (groundedLastFrame)
+				{
+					lastGroundedVelocity = new Vector3(Velocity.X, 0, Velocity.Z);
+				}
+				groundedLastFrame = false;
+
+				Velocity = new Vector3(lastGroundedVelocity.X, Velocity.Y, lastGroundedVelocity.Z);
+				*/
+			}
+
+			MoveAndSlide();
+		}
+
+		public void Die()
+		{
+			GD.Print("[Player] Died!");
+			GameOver.Open();
+			// levelManager.ChangeLevel("Main_Menu");
+		}
+	}
 
 }
-
--- Game/Demo/Script/EatAndLungeAbility.cs
+++ Game/Demo/Script/EatAndLungeAbility.cs
@@ -4,94 +4,99 @@ using Godot.Collections;
 
 public partial class EatAndLungeAbility : Node
 {
-    [Export(PropertyHint.None, "suffix:m")] public float RayLength = 30.0f;
-    [Export] public BellyDisplay BellyDisplay;
-    [Export] public CharacterController characterController;
+	[Export(PropertyHint.None, "suffix:m")] public float RayLength = 30.0f;
+	[Export] public BellyDisplay BellyDisplay;
+	[Export] public CharacterController characterController;
 
-    public bool Gobbling { get; set; } = false;
+	public bool Gobbling { get; set; } = false;
 
-    private bool bellyIsFull = false;
-    private CameraBridge cameraBridge;
-    private SFX sfx;
+	private bool bellyIsFull = false;
+	private CameraBridge cameraBridge;
+	private SFX sfx;
 
-    public override void _Ready()
-    {
-        cameraBridge = GetNode<CameraBridge>("/root/CameraBridge");
-        sfx = GetNode<SFX>("/root/SFX");
-        sfx.PlaySound("Bubbles", new Vector3(0,0,0));
-    }
+	public override void _Ready()
+	{
+		cameraBridge = GetNode<CameraBridge>("/root/CameraBridge");
+		sfx = GetNode<SFX>("/root/SFX");
+		sfx.PlaySound("Bubbles", new Vector3(0,0,0));
+	}
 
-    private float gobbleTimer = 0;
-    private float gobbleTimerMax = 1;
+	private float gobbleTimer = 0;
+	private float gobbleTimerMax = 1;
 
-    public override void _Process(double delta)
-    {
-        float dt = (float)delta;
+	public override void _Process(double delta)
+	{
+		float dt = (float)delta;
 
-        if (Gobbling)
-        {
-            gobbleTimer += dt;
+		if (Gobbling)
+		{
+			if (characterController.IsOnFloor())
+			{
+				Gobbling = false;
+			}
+			/*
+			gobbleTimer += dt;
 
-            if (gobbleTimer > gobbleTimerMax)
-            {
-                Gobbling = false;
-            }
-        }
-    }
+			if (gobbleTimer > gobbleTimerMax)
+			{
+				Gobbling = false;
+			}
+			*/
+		}
+	}
 
-    public override void _UnhandledInput(InputEvent inputEvent)
-    {
-        if (inputEvent.IsActionPressed("left_click"))
-        {
-            if (bellyIsFull)
-            {
-                GD.Print("Launch!!");
+	public override void _UnhandledInput(InputEvent inputEvent)
+	{
+		if (inputEvent.IsActionPressed("left_click"))
+		{
+			if (bellyIsFull)
+			{
+				GD.Print("Launch!!");
 
-                Vector3 launchDirection = -cameraBridge.MainCamera.GlobalTransform.Basis.Z;
-                float launchForce = 20.0f;
-                characterController.Velocity += launchDirection * launchForce;
-                BellyDisplay.Empty();
-                bellyIsFull = false;
-                Gobbling = true;
-                gobbleTimer = 0;
-            }
-            else
-            {
-                PhysicsDirectSpaceState3D spaceState = GetViewport().GetWorld3D().DirectSpaceState;
-                Vector2 mousePos = GetViewport().GetMousePosition();
-                Vector3 origin = cameraBridge.MainCamera.ProjectRayOrigin(mousePos);
-                Vector3 end = origin + cameraBridge.MainCamera.ProjectRayNormal(mousePos) * RayLength;
-                PhysicsRayQueryParameters3D query = PhysicsRayQueryParameters3D.Create(origin, end);
-                query.CollisionMask = 1u << 14;
-                Dictionary result = spaceState.IntersectRay(query);
+				Vector3 launchDirection = -cameraBridge.MainCamera.GlobalTransform.Basis.Z;
+				float launchForce = 20.0f;
+				characterController.Velocity += launchDirection * launchForce;
+				BellyDisplay.Empty();
+				bellyIsFull = false;
+				Gobbling = true;
+				gobbleTimer = 0;
+			}
+			else
+			{
+				PhysicsDirectSpaceState3D spaceState = GetViewport().GetWorld3D().DirectSpaceState;
+				Vector2 mousePos = GetViewport().GetMousePosition();
+				Vector3 origin = cameraBridge.MainCamera.ProjectRayOrigin(mousePos);
+				Vector3 end = origin + cameraBridge.MainCamera.ProjectRayNormal(mousePos) * RayLength;
+				PhysicsRayQueryParameters3D query = PhysicsRayQueryParameters3D.Create(origin, end);
+				query.CollisionMask = 1u << 14;
+				Dictionary result = spaceState.IntersectRay(query);
 
-                if (result.ContainsKey("position"))
-                {
-                    GD.Print($"Hit position: {result["position"]}");
+				if (result.ContainsKey("position"))
+				{
+					GD.Print($"Hit position: {result["position"]}");
 
-                    CharacterBody3D hitBody = (CharacterBody3D)result["collider"];
+					CharacterBody3D hitBody = (CharacterBody3D)result["collider"];
 
-                    if (hitBody != null && hitBody is AgentBase agentBase)
-                    {
-                        agentBase.Die();
-                        BellyDisplay.Full();
-                        bellyIsFull = true;
-                        return;
-                    }
-                    else
-                    {
+					if (hitBody != null && hitBody is AgentBase agentBase)
+					{
+						agentBase.Die();
+						BellyDisplay.Full();
+						bellyIsFull = true;
+						return;
+					}
+					else
+					{
 
-                        GD.Print(hitBody.GetType());
-                    }
-                }
-                else
-                {
-                    GD.Print("Miss");
-                }
-            }            
-        }
+						GD.Print(hitBody.GetType());
+					}
+				}
+				else
+				{
+					GD.Print("Miss");
+				}
+			}            
+		}
 
-    }
+	}
 
 }
-
